b8006545df64343afdd9004a13f1373fb659574c
Trang Nguyen <nguyenminhtrang9213@gmail.com>
2021-12-05 15:02:00 +0100

    final app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Review from "./components/Review";
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title">our reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </main>
+  );
+}
+
+export default App;
